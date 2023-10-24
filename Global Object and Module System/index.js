2987be3f2c343ec151bf4e6a84d7e3b53b9a8e82
kawsarkabir <devkawsarkabir@gmail.com>
2023-10-24 15:00:22 +0600

    global and modules systems

---
+++ Global Object and Module System/index.js
@@ -0,0 +1,11 @@
+/* console.log(global); */
+/* setTimeout((=>{
+    console.log('kawsar kabir');
+}) 1000) */
+
+/* console.log(__dirname);
+console.log(__filename) */
+var people = require("./people");
+const _  = require('lodash')
+
+console.log(_.last(people));
---
+++ Global Object and Module System/people.js
@@ -0,0 +1,4 @@
+var people = ['sakib', 'tamim', 'masrafe']
+// module.exports = people
+module.exports = people
+console.log(module);
